7da041c0dd5bbb16c1cb0daf1c27bc7dd387303d
Cars Chandler <carschandler7@gmail.com>
2024-06-16 01:58:57 -0500

    Initial commit

---
+++ devshell/flake.nix
@@ -0,0 +1,37 @@
+{
+  description = "R Development Shell";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs = { nixpkgs, ... }:
+    let
+      pkgs = nixpkgs.legacyPackages.x86_64-linux;
+      # rstudio = pkgs.rstudioWrapper;
+      # rstudio = pkgs.rstudio;
+    in {
+        devShells.x86_64-linux.default = pkgs.mkShell {
+          packages = [ 
+            # pkgs.rWrapper.override {
+            #   packages = [
+            #     pkgs.rPackages.alr4
+            #     pkgs.rPackages.leaps
+            #     pkgs.rPackages.SuppDists
+            #     pkgs.rPackages.rmarkdown
+            #   ];
+            # }
+
+            # pkgs.rPackages.alr4
+            # pkgs.rPackages.leaps
+            # pkgs.rPackages.SuppDists
+            # pkgs.rPackages.rmarkdown
+            pkgs.rstudio
+            # pkgs.texliveFull
+            # pkgs.pandoc
+          ];
+        };
+
+        formatter.x86_64-linux = nixpkgs.legacyPackages.x86_64-linux.nixpkgs-fmt;
+    };
+}
